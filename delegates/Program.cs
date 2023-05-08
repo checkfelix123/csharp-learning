7e9c5154a74fe8368a36b846974fff4586537d1f
graff <grafservices@gmx.at>
2023-05-08 21:46:28 +0200

    init commit

---
+++ delegates/Program.cs
@@ -0,0 +1,65 @@
+﻿using delegates;
+using System.Timers;
+
+public delegate void ProgramConverterCallback(double value);
+public delegate void TimerEmittedValueCallback(string s);
+public delegate void TemperatureTimerCallback(Temperature s);
+public class TemperatureSensor
+{
+    public event ProgramConverterCallback EmittedValueCallback;
+
+    public event TemperatureTimerCallback TemperatureTimerCallback;
+
+
+
+
+    public TemperatureSensor(ProgramConverterCallback pic)
+    {
+        this.EmittedValueCallback = pic;
+       
+
+        System.Timers.Timer timer = new System.Timers.Timer(5000);
+
+        timer.Elapsed += (object? sender, ElapsedEventArgs elapsedEventArgs) =>
+        {
+            Random random = new Random();
+            double d;
+           if(Double.TryParse(random.Next(0, 100).ToString(), out d))
+            {
+                this.EmittedValueCallback?.Invoke(d);
+            }
+
+           Temperature t = new Temperature(((random.NextDouble() * 20.0) + 4.0), "Traismauer");
+            this.TemperatureTimerCallback?.Invoke(t);
+
+        };
+        timer.Start();
+
+
+    }
+
+    static void Main(String[] args)
+    {
+        //cration of a TemperatureSenson
+        TemperatureSensor p = new TemperatureSensor((double value) =>
+        {
+            Console.WriteLine("converted Value " + value * 3);
+        });
+        //listening to the callback
+        p.EmittedValueCallback += (double d) =>
+        {
+            Console.WriteLine("original value: " + d);
+        };
+
+        // listening to the other callback which emitts the Temperature
+        p.TemperatureTimerCallback += (Temperature t) => {
+            Console.WriteLine("Emitted Temperature from sensor: " + t);
+        };
+
+
+        Console.ReadLine();
+
+    }
+
+    
+}
---
+++ delegates/Temperature.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace delegates
+{
+    public class Temperature
+    {
+        double CurrentValue { get; set; } = 0;
+        String Region { get; set; } = "";
+
+        public Temperature(double temp, string region) {
+            CurrentValue = temp;
+            Region = region;    
+        }
+
+        public override string? ToString()
+        {
+            return "Current Value: " + CurrentValue + " Region: " + Region;
+        }
+    }
+}
